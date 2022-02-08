16933fdb8d9580f76fba0c3b423a75aa793c823f
Daniil Karpovych <kadavi444@gmail.com>
2022-02-08 10:52:55 +0200

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint'
+  ],
+  rules: {
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error']
+  }
+}
